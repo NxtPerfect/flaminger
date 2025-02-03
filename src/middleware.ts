5a3687dfa17af38300bb32faf5fa58005b18bd35
NxtPerfect <alakaxan@protonmail.com>
2025-02-03 10:32:45 +0100

    feat: added middleware
    feat: protected profile, login, register and offer apply routes

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const hasSession = request.cookies.has("session");
+  const isLogin = request.nextUrl.pathname.startsWith('/login');
+  const isRegister = request.nextUrl.pathname.startsWith('/register');
+  const isOffer = request.nextUrl.pathname.startsWith('/offer');
+  const isProfile = request.nextUrl.pathname.startsWith('/profile');
+
+  console.log(hasSession, isLogin, isRegister, isOffer, isProfile);
+
+  if (hasSession && (isLogin || isRegister)) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+  if (!hasSession && (isOffer || isProfile)) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/profile', '/login', '/register', '/offer/:id/apply'],
+}
