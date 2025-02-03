426798145c040a77cc1ab6298f0bb2ebf63b5bed
NxtPerfect <alakaxan@protonmail.com>
2025-02-03 10:33:22 +0100

    feat: moved type definitions to separate file

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -14,3 +14,43 @@ export type SessionPayload = {
   userId: string
   expiresAt: Date
 }
+
+export type Requirement = {
+  languages: string
+  minimumExperienceInYears: number
+}
+
+export type OfferWithCompanyInfo = {
+  jobs_table: Offer
+  companies_table: Company
+  jobs_to_users_table: UserApplications
+}
+
+export type Offer = {
+  id: number
+  byCompanyId: number
+  title: string
+  description: string
+  company: string
+  isClosed: boolean
+  // requirements: Array<Requirement>
+  // state: "new" | "applied" | "accepted" | "rejected"
+}
+
+export type Company = {
+  id: number
+  name: string
+  jobsAccepted: number
+  jobsRejected: number
+  acceptanceRate: number
+}
+
+export type UserApplications = {
+  userId: number
+  jobId: number
+  isApplied: boolean
+  isApplicationInProgress: boolean
+  isAccepted: boolean
+  rejectionReason: string
+}
+
