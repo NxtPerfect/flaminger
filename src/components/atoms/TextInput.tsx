f5dce90529de1ef6a206d180671e085872729fcf
NxtPerfect <alakaxan@protonmail.com>
2025-02-17 08:32:53 +0100

    feat: added autocompletion list to input

--- src/components/atoms/TextInput.tsx
+++ src/components/atoms/TextInput.tsx
@@ -10,14 +10,15 @@ type Props = {
   value?: string
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
   defaultValue?: string
+  list?: string
 }
 
-export default function TextInput({ className, name, placeholder, required, pattern, children, onChange, defaultValue }: Props) {
+export default function TextInput({ className, name, placeholder, required, pattern, children, onChange, defaultValue, list }: Props) {
   const style = `w-[100%] rounded-md px-2 py-1 text-black invalid:[&:not(:placeholder-shown):not(:focus)]:bg-red-300/80 ${className}`.trim();
   return (
     <div className="flex flex-col">
       <label htmlFor={name}>{required && "*"}{children}</label>
-      <input type={name === "email" ? "email" : "text"} name={name} placeholder={placeholder} pattern={pattern} required={required} className={style} onChange={onChange} defaultValue={defaultValue} />
+      <input type={name === "email" ? "email" : "text"} name={name} placeholder={placeholder} pattern={pattern} required={required} className={style} onChange={onChange} defaultValue={defaultValue} list={list}/>
     </div>
   )
 }
