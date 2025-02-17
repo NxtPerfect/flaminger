9d87f3a087005e20a04c0ed5572258f4fd1dc47f
NxtPerfect <alakaxan@protonmail.com>
2025-02-17 10:37:35 +0100

    fix: added proper form values to questions

--- src/components/organisms/ApplyToJobForm.tsx
+++ src/components/organisms/ApplyToJobForm.tsx
@@ -34,36 +34,36 @@ export default function ApplyToJobForm({ jobId = 0 }: Props) {
         Do you use AI Tools?
       </label>
       <div>
-        <input type="checkbox" />
+        <input type="checkbox" name="ai" />
         Yes
       </div>
       <label>
         Are you willing to relocate?
       </label>
       <div className="flex flex-row gap-2">
-        <input type="radio" value="yes" />
+        <input type="radio" value="yes" name="relocation" />
         <label>Yes</label>
       </div>
       <div className="flex flex-row gap-2">
-        <input type="radio" value="no" />
+        <input type="radio" value="no" name="relocation" />
         <label>No</label>
       </div>
       <label>
         Type of employment you want:
       </label>
-      <select>
-        <option>Remote</option>
-        <option>Hybrid</option>
-        <option>Stationary</option>
+      <select name="employmentType">
+        <option value="remote">Remote</option>
+        <option value="hybrid">Hybrid</option>
+        <option value="stationary">Stationary</option>
       </select>
       <label>
         Describe briefly why you should be accepted for this position.
       </label>
-      <input type="text" minLength={1} />
+      <input type="text" minLength={1} name="description" />
       <label>
         For human verification, what is the name for an animal whose name is duck:
       </label>
-      <input type="text" maxLength={16} />
+      <input type="text" maxLength={16} name="verification" />
       <ActionButton variant="formSubmit">Apply</ActionButton>
     </form>
   )
