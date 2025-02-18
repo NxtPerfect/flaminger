b447bf7adb0e863c50034e10504b46d85f9fd34d
NxtPerfect <alakaxan@protonmail.com>
2025-02-18 08:35:27 +0100

    feat: added left to right movement animation for spinner

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,16 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        'left-right': {
+          '0%': { transform: 'scale(100%) translateX(-100%)' },
+          '50%': { transform: 'scale(400%) translateX(165%)' },
+          '100%': { transform: 'scale(100%) translateX(1500%)' },
+        }
+      },
+      animation: {
+        'left-right': 'left-right 2s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
