eb1707487d62fba8bb6e99e45bbf607d1b47bfdd
NxtPerfect <alakaxan@protonmail.com>
2025-02-18 08:35:13 +0100

    fix: email pattern properly escaped

--- src/components/molecules/EmailInput.tsx
+++ src/components/molecules/EmailInput.tsx
@@ -3,7 +3,7 @@ import TextInput from '../atoms/TextInput'
 
 export default function EmailInput() {
   return (
-    <TextInput name="email" placeholder="sample@email.io" required={true} pattern="\S+@\S+\.\S+">
+    <TextInput name="email" placeholder="sample@email.io" required={true} pattern={`\\S+@\\S+\\.\\S+`}>
       Email address
     </TextInput>
   )
