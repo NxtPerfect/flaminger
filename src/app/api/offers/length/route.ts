21c591f58bfda0d3019d1497ed1162aa7215922c
NxtPerfect <alakaxan@protonmail.com>
2025-03-03 09:12:00 +0100

    feat: removed unused param from api route

--- src/app/api/offers/length/route.ts
+++ src/app/api/offers/length/route.ts
@@ -1,7 +1,7 @@
 import { MAX_JOBS_PER_PAGE } from "@/app/lib/definitions";
 import { getJobsMaxPages } from "@/db/queries/select";
 
-export async function GET(req: Request) {
+export async function GET() {
   const [{ count }] = await getJobsMaxPages();
   return Response.json(
     { count: Math.ceil(count / MAX_JOBS_PER_PAGE) },
