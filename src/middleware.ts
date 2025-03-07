f4f1a11a184b73ac2898f9e1858f3fdd4e7f0c6a
NxtPerfect <alakaxan@protonmail.com>
2025-03-07 06:08:13 +0100

    fix: /applications and /offer/add routes blocked for non-employers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,34 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { getUserId } from './app/lib/session';
+import { getUserById } from './db/queries/select';
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const hasSession = request.cookies.has("session");
   const isLogin = request.nextUrl.pathname.startsWith('/login');
   const isRegister = request.nextUrl.pathname.startsWith('/register');
   const isOffer = request.nextUrl.pathname.startsWith('/offer');
   const isProfile = request.nextUrl.pathname.startsWith('/profile');
+  const isApplicants = request.nextUrl.pathname.startsWith('/applicants');
+  console.log("Pathname", request.nextUrl.pathname);
 
   if (hasSession && (isLogin || isRegister)) {
     return NextResponse.redirect(new URL('/', request.url));
   }
-  if (!hasSession && (isOffer || isProfile)) {
+  if (!hasSession && (isOffer || isProfile || isApplicants)) {
     return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  const userId = await getUserId();
+  const [user] = await getUserById(userId);
+  if (!user.isEmployer || !user.employerCompanyId) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/profile', '/login', '/register', '/offer/:id/apply'],
+  matcher: ['/profile', '/login', '/register', '/offer/:id/apply',
+    '/offer/add', '/applicants'],
 }
