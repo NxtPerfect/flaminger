cf14ccd74a27d77057ad00232f62a1e7a17a2b6e
NxtPerfect <alakaxan@protonmail.com>
2025-03-14 09:14:49 +0100

    feat: adjusted skeleton job offer for new layout

--- src/components/placeholders/SkeletonJobOffer.tsx
+++ src/components/placeholders/SkeletonJobOffer.tsx
@@ -2,27 +2,28 @@ import React from 'react'
 
 export default function SkeletonJobOffer() {
   return (
-    <div className="flex flex-col bg-neutral-900 rounded-md p-4 px-8 min-w-[35svw] min-h-36 animate-pulse">
-      <span className="flex flex-row gap-2">
+    <div className="flex flex-col bg-neutral-900 rounded-md p-4 px-8 min-w-[35svw] max-w-[45svw] min-h-36 animate-pulse">
+      <div className="flex flex-row gap-2">
         <div className="min-w-full min-h-6 bg-neutral-700"></div>
-      </span>
-      <span className="mt-4 flex flex-row gap-2">
+      </div>
+      <div className="mt-4 flex flex-row gap-2">
         <div className="min-w-32 min-h-6 bg-neutral-700"></div>
-      </span>
-      <span className="mt-4 w-[50ch] h-[3lh] text-ellipsis bg-neutral-700"></span>
-      <span className="mt-4 flex flex-row items-center justify-between">
-        <span className="flex flex-row gap-2 align-middle items-center">
+      </div>
+      <div className="mt-4 h-[3lh] text-ellipsis bg-neutral-700"></div>
+      <div className="mt-4 h-[3lh] text-ellipsis bg-neutral-700"></div>
+      <div className="mt-4 flex flex-row items-center justify-between">
+        <div className="flex flex-row gap-2 align-middle items-center">
           <div className="min-w-32 min-h-8 bg-neutral-700 rounded-md px-1 self-center">
           </div>
           <div className="min-w-16 min-h-8 bg-neutral-700 rounded-md px-1 self-center">
           </div>
           <div className="min-w-24 min-h-8 bg-neutral-700 rounded-md px-1 self-center">
           </div>
-        </span>
-        <span className="flex flex-row gap-8">
+        </div>
+        <div className="flex flex-row gap-8">
           <div className="min-w-48 min-h-8 bg-neutral-700"></div>
-        </span>
-      </span>
+        </div>
+      </div>
     </div>
   )
 }
