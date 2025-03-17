e2137bdb0e1fd5d9d6788a324cf04caf38652ec6
NxtPerfect <alakaxan@protonmail.com>
2025-03-17 07:32:31 +0100

    feat: added useUserProfile hook for user profile data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,128 +1,36 @@
 "use client";
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import SkeletonProfile from '@/components/placeholders/SkeletonProfile';
-import { CompletedApplication, DatabaseCompletedApplication, HumanLanguage, Offer, PendingApplication, StatisticsForUserApplications, StatisticsForUserApplicationsFromDatabase, Technology, User } from '@/app/lib/definitions';
+import { ROLE_VARIANTS, ROLES } from '@/app/lib/definitions';
 import JobApplicationsForUserProfile from '@/components/organisms/JobApplicationsForUserProfile';
 import UserLanguages from '@/components/organisms/UserLanguages';
 import UserTechnologies from '@/components/organisms/UserTechnologies';
 import StatisticsUserPieChart from '@/components/organisms/StatisticsUserPieChart';
 import LinkButton from '@/components/organisms/LinkButton';
 import ProfilePicture from '@/components/atoms/ProfilePicture';
+import { useAuth } from '@/hooks/useAuth';
+import { useUserProfile } from '@/hooks/useUserProfile';
 
 export default function Profile() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [user, setUser] = useState<User>();
-  const [statistics, setStatistics] = useState<StatisticsForUserApplications>();
-  const [pendingApplications, setPendingApplications] = useState<Partial<Offer>[]>([]);
-  const [completedApplications, setCompletedApplications] = useState<CompletedApplication[]>([]);
-  const [languages, setLanguages] = useState<HumanLanguage[]>([]);
-  const [technologies, setTechnologies] = useState<Technology[]>([]);
-
-  const getProfile = useCallback(async () => {
-    setIsLoading(true);
-    await fetch('/api/profile', { method: "POST" })
-      .then(async (res) => {
-        const responseJson = await res.json();
-        setUserDataFromApi(responseJson.userData);
-        setStatisticsFromApi(responseJson.statistics);
-        setPendingApplicationsFromApi(responseJson.pendingApplications);
-        setCompletedApplicationsFromApi(responseJson.completedApplications);
-        setLanguagesFromApi(responseJson.humanLanguages);
-        setTechnologiesFromApi(responseJson.technologies);
-        setIsLoading(false);
-      })
-  }, []);
-
-  useEffect(() => {
-    getProfile();
-  }, [getProfile]);
-
-  function setUserDataFromApi(rawUserData: User) {
-    const userData: User = {
-      id: rawUserData.id,
-      firstname: rawUserData.firstname,
-      surname: rawUserData.surname,
-      password: "",
-      email: "",
-      mailingConsent: rawUserData.mailingConsent,
-      isEmployer: rawUserData.isEmployer
-    }
-    setUser(userData);
-  }
-
-  function setStatisticsFromApi(rawStatistics: StatisticsForUserApplicationsFromDatabase) {
-    const statisticsForUser: StatisticsForUserApplications = {
-      accepted: rawStatistics.accepted.count,
-      rejected: rawStatistics.rejected.count,
-      total: rawStatistics.total.count,
-      responseRate: ((rawStatistics.accepted.count + rawStatistics.rejected.count) / (rawStatistics.total.count) * 100).toFixed(2)
-    }
-    setStatistics(statisticsForUser);
-  }
-
-  function setPendingApplicationsFromApi(rawPendingApplications: PendingApplication[]) {
-    for (let i = 0; i < rawPendingApplications.length; i++) {
-      const singleApplication: Partial<Offer> = rawPendingApplications[i].jobsTable;
-      const companyName = rawPendingApplications[i].company;
-      const application: Partial<Offer> = {
-        id: singleApplication.id,
-        company: companyName,
-        title: singleApplication.title,
-        description: singleApplication.description,
-        byCompanyId: singleApplication.byCompanyId,
-        isClosed: false
-      }
-      setPendingApplications((pending) => [...pending, application]);
-    }
-  }
-
-  function setCompletedApplicationsFromApi(rawCompletedApplications: DatabaseCompletedApplication[]) {
-    for (let i = 0; i < rawCompletedApplications.length; i++) {
-      const singleApplication: Offer = rawCompletedApplications[i].jobsTable;
-      const isAccepted = rawCompletedApplications[i].isAccepted;
-      const rejectionReason = rawCompletedApplications[i].rejectionReason;
-      const companyName = rawCompletedApplications[i].company;
-      const application: CompletedApplication = {
-        id: singleApplication.id,
-        company: companyName,
-        title: singleApplication.title,
-        description: singleApplication.description,
-        byCompanyId: singleApplication.byCompanyId,
-        isAccepted: isAccepted,
-        rejectionReason: rejectionReason,
-      };
-      setCompletedApplications((completed) => [...completed, application]);
-    }
-  }
-
-  function setLanguagesFromApi(rawLanguages: HumanLanguage[]) {
-    for (let i = 0; i < rawLanguages.length; i++) {
-      const language: HumanLanguage = {
-        name: rawLanguages[i].name,
-        level: rawLanguages[i].level,
-      }
-      setLanguages((current) => [...current, language]);
-    }
-  }
-
-  function setTechnologiesFromApi(rawTechnologies: Technology[]) {
-    for (let i = 0; i < rawTechnologies.length; i++) {
-      const tech: Technology = {
-        name: rawTechnologies[i].name,
-        experience: rawTechnologies[i].experience,
-      }
-      setTechnologies((current) => [...current, tech]);
-    }
-  }
+  const {
+    isLoading,
+    user,
+    statistics,
+    pendingApplications,
+    completedApplications,
+    languages,
+    technologies
+  } = useUserProfile();
+  const { role } = useAuth();
 
   return (
     <div>
-      {isLoading ?
-        (<SkeletonProfile />)
-        :
-        (<>
-          <div className="flex flex-col rounded-2xl bg-neutral-800/60 p-4
+      {isLoading &&
+        <SkeletonProfile />}
+      <div className="flex flex-col rounded-2xl bg-neutral-800/60 p-4
             text-white items-center">
+        {role === ROLES[ROLE_VARIANTS.user] &&
+          <>
             <div className="grid grid-cols-2 grid-rows-2 gap-4 justify-items-center items-center w-[80svw] md:w-[50svw] p-4">
               <ProfilePicture className="" />
               <div className="self-end justify-self-end">
@@ -146,10 +54,12 @@ export default function Profile() {
               </div>
             </div>
             <JobApplicationsForUserProfile pendingApplications={pendingApplications} completedApplications={completedApplications} />
-          </div>
-        </>
-        )
-      }
+          </>
+        }
+      </div>
+      <div>
+        Not a user
+      </div>
     </div>
   )
 }
---
+++ src/hooks/useUserProfile.ts
@@ -0,0 +1,129 @@
+import { CompletedApplication, DatabaseCompletedApplication, HumanLanguage, Offer, PendingApplication, StatisticsForUserApplications, StatisticsForUserApplicationsFromDatabase, Technology, User } from "@/app/lib/definitions";
+import { useCallback, useEffect, useState } from "react";
+
+type UserProfile = {
+  isLoading: boolean
+  user: User
+  statistics: StatisticsForUserApplications
+  pendingApplications: Partial<Offer>[]
+  completedApplications: CompletedApplication[]
+  languages: HumanLanguage[]
+  technologies: Technology[]
+}
+
+export function useUserProfile(): UserProfile {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [user, setUser] = useState<User>();
+  const [statistics, setStatistics] = useState<StatisticsForUserApplications>();
+  const [pendingApplications, setPendingApplications] = useState<Partial<Offer>[]>([]);
+  const [completedApplications, setCompletedApplications] = useState<CompletedApplication[]>([]);
+  const [languages, setLanguages] = useState<HumanLanguage[]>([]);
+  const [technologies, setTechnologies] = useState<Technology[]>([]);
+
+  const getProfile = useCallback(async () => {
+    setIsLoading(true);
+    await fetch('/api/profile', { method: "POST" })
+      .then(async (res) => {
+        const responseJson = await res.json();
+        setUserDataFromApi(responseJson.userData);
+        setStatisticsFromApi(responseJson.statistics);
+        setPendingApplicationsFromApi(responseJson.pendingApplications);
+        setCompletedApplicationsFromApi(responseJson.completedApplications);
+        setLanguagesFromApi(responseJson.humanLanguages);
+        setTechnologiesFromApi(responseJson.technologies);
+        setIsLoading(false);
+      })
+  }, []);
+
+  useEffect(() => {
+    getProfile();
+  }, [getProfile]);
+
+  function setUserDataFromApi(rawUserData: User) {
+    const userData: User = {
+      id: rawUserData.id,
+      firstname: rawUserData.firstname,
+      surname: rawUserData.surname,
+      password: "",
+      email: "",
+      mailingConsent: rawUserData.mailingConsent,
+      isEmployer: rawUserData.isEmployer
+    }
+    setUser(userData);
+  }
+
+  function setStatisticsFromApi(rawStatistics: StatisticsForUserApplicationsFromDatabase) {
+    const statisticsForUser: StatisticsForUserApplications = {
+      accepted: rawStatistics.accepted.count,
+      rejected: rawStatistics.rejected.count,
+      total: rawStatistics.total.count,
+      responseRate: ((rawStatistics.accepted.count + rawStatistics.rejected.count) / (rawStatistics.total.count) * 100).toFixed(2)
+    }
+    setStatistics(statisticsForUser);
+  }
+
+  function setPendingApplicationsFromApi(rawPendingApplications: PendingApplication[]) {
+    for (let i = 0; i < rawPendingApplications.length; i++) {
+      const singleApplication: Partial<Offer> = rawPendingApplications[i].jobsTable;
+      const companyName = rawPendingApplications[i].company;
+      const application: Partial<Offer> = {
+        id: singleApplication.id,
+        company: companyName,
+        title: singleApplication.title,
+        description: singleApplication.description,
+        byCompanyId: singleApplication.byCompanyId,
+        isClosed: false
+      }
+      setPendingApplications((pending) => [...pending, application]);
+    }
+  }
+
+  function setCompletedApplicationsFromApi(rawCompletedApplications: DatabaseCompletedApplication[]) {
+    for (let i = 0; i < rawCompletedApplications.length; i++) {
+      const singleApplication: Offer = rawCompletedApplications[i].jobsTable;
+      const isAccepted = rawCompletedApplications[i].isAccepted;
+      const rejectionReason = rawCompletedApplications[i].rejectionReason;
+      const companyName = rawCompletedApplications[i].company;
+      const application: CompletedApplication = {
+        id: singleApplication.id,
+        company: companyName,
+        title: singleApplication.title,
+        description: singleApplication.description,
+        byCompanyId: singleApplication.byCompanyId,
+        isAccepted: isAccepted,
+        rejectionReason: rejectionReason,
+      };
+      setCompletedApplications((completed) => [...completed, application]);
+    }
+  }
+
+  function setLanguagesFromApi(rawLanguages: HumanLanguage[]) {
+    for (let i = 0; i < rawLanguages.length; i++) {
+      const language: HumanLanguage = {
+        name: rawLanguages[i].name,
+        level: rawLanguages[i].level,
+      }
+      setLanguages((current) => [...current, language]);
+    }
+  }
+
+  function setTechnologiesFromApi(rawTechnologies: Technology[]) {
+    for (let i = 0; i < rawTechnologies.length; i++) {
+      const tech: Technology = {
+        name: rawTechnologies[i].name,
+        experience: rawTechnologies[i].experience,
+      }
+      setTechnologies((current) => [...current, tech]);
+    }
+  }
+
+  return {
+    isLoading,
+    user,
+    statistics,
+    pendingApplications,
+    completedApplications,
+    languages,
+    technologies
+  }
+}
